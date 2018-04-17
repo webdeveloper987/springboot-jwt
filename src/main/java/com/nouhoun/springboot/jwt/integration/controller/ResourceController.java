34cf0bf587fa96cff01e42fa355133ec057e29b9
alemd <webdeveloper987@users.noreply.github.com>
2018-04-17 18:10:25 +0200

    Update ResourceController.java

--- src/main/java/com/nouhoun/springboot/jwt/integration/controller/ResourceController.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/controller/ResourceController.java
@@ -5,10 +5,7 @@ import com.nouhoun.springboot.jwt.integration.domain.User;
 import com.nouhoun.springboot.jwt.integration.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
